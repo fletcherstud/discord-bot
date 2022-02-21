3cedca392b6e6a9e7d6da572875f83b05a91c88a
Jonathan Fletcher <johnfletcher831@gmail.com>
2022-02-21 12:11:03 -0700

    Changed reaction emoji to custom emoji

--- role-claim.js
+++ role-claim.js
@@ -5,9 +5,9 @@ const { MessageEmbed } = require('discord.js');
 module.exports = client => { 
     const channelId = `930708332535238676`
     const launchDate = new Date(1647568840000); // Date to get OG role
-    let emojiText = '1. No spam or self promotion\n2. No doxxing one another\n3. Use the correct channels - ask a moderator for help if needed\n4. Be respectful\n5. No NSFW or obscene content\n6. Report any malicious behaviour to the moderators \n\nAccept our rules by reacting with the checkmark emoji below!\n\n';
-
-    const reactions = ["✅"];
+    let emojiText = '1. No spam or self promotion\n2. No doxxing one another\n3. Use the correct channels - ask a moderator for help if needed\n4. Be respectful\n5. No NSFW or obscene content\n6. Report any malicious behaviour to the moderators \n\nAccept our rules by reacting to our custom emoji below!\n\n';
+    const emoji = client.emojis.cache.get("945393635913007155");
+    const reactions = [emoji.toString()];
 
     firstMessage(client, channelId, emojiText, reactions);
     
