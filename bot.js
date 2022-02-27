efa54e21b8f2ecaed0549abdc4a32dbf2e3193a1
Jonathan Fletcher <johnfletcher831@gmail.com>
2022-02-26 19:27:01 -0700

    Added logger for debuging

--- bot.js
+++ bot.js
@@ -76,6 +76,7 @@ bot.on("messageCreate", function(message) {
 })
 
 stream.on('tweet', tweet => {
+    logger.info(tweet)
     if (tweet.in_reply_to_status_id != null || tweet.in_reply_to_status_id_str != null || tweet.in_reply_to_user_id != null|| tweet.in_reply_to_user_id_str != null || tweet.in_reply_to_screen_name != null){
         logger.info("Incoming tweet is a reply... ignoring");
         return false;
